d4e774ac978821a2c8128ea022594086cd136c8a
Troy Shu <tmshu1@gmail.com>
2012-11-06 15:51:33 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Maw::Application.config.secret_token = 'e7c1bbb2b8495ab0330b0840831d1f197953a2a5d2291c99f327e9a50f598d41b91a965bf06cdec2e77ab9b92e68854e7181ac70d5ef8d798fffffbed0a17ae7'
